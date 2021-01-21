11b49933ace237e5e66c0f8581eee49b3869ca2b
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-01-20 19:32:34 -0600

    add pagination to recipe page

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ const closeBtn = document.querySelector('.closebtn');
 const footer = document.querySelector('#footer');
 const youtube = document.querySelector('.youtube');
 const recipesSearch = document.querySelector('.recipes-search');
-const recipeButton = document.querySelectorAll('.recipe-button');
 
+// Load first page of recipes with pagination if user is on recipes.html
+if (document.URL.includes("recipes.html")) {
+    showPage(recipes, 1);
+    addPagination(recipes);
+}
 
 /* ============================================= */
 /*              Functions                        */
@@ -21,9 +25,11 @@ function openNav() {
     mobileNav.style.width = "100%";
     main.style.marginRight = "100%";
     footer.style.display = 'none';
+
     if (youtube) {
         youtube.style.display = 'none';
     }
+
     navLogo.style.display = 'none';
     navMenu.style.display = 'none';
 }
@@ -33,9 +39,11 @@ function closeNav() {
     mobileNav.style.width = "0";
     main.style.marginRight = "0";
     footer.style.display = 'block';
+
     if (youtube) {
         youtube.style.display = 'block';
     }
+
     navLogo.style.display = 'block';
 }
 
@@ -63,19 +71,89 @@ function blackNavOnScroll() {
 }
 
 // Search for recipes dynamically as user types
-function searchRecipes() {
-    const userInput = recipesSearch.value.toLowerCase();
-    const cards = document.querySelectorAll(".featured-img-pic");
+function searchRecipes(e) {
+    const eTargetValue = e.target.value.toLowerCase();
 
-    cards.forEach((recipe, index) => {
-        const recipeTitles = recipe.firstElementChild.lastElementChild.firstElementChild.firstElementChild.textContent.toLowerCase();
+    let recipeArr = recipes.filter(recipe => {
+        const recipeName = recipe.name.toLowerCase();
+        return recipeName.includes(eTargetValue);
+    })
 
-        if (recipeTitles.includes(userInput)) {
-            cards[index].style.display = "flex";
-        } else {
-            cards[index].style.display = "none";
+    showPage(recipeArr, 1);
+    addPagination(recipeArr);
+}
+
+// Add pagination buttons based on page load and search results
+function addPagination(list) {
+    const numOfPaginationBtns = Math.ceil(list.length / 9);
+    const linkList = document.querySelector('.link-list');
+    linkList.innerHTML = '';
+
+    for (let i = 1; i <= numOfPaginationBtns; i++) {
+        linkList.insertAdjacentHTML("beforeend",
+            `<li>
+                <button type="button">${i}</button>
+            </li>`);
+    }
+
+    if (linkList.firstElementChild) {
+        const firstLiItem = linkList.firstElementChild.firstElementChild;
+        firstLiItem.classList.add('active');
+        firstLiItem.addEventListener('click', () => {
+            window.location.reload();
+        });
+    }
+
+    linkList.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+            const linkListButtons = document.querySelectorAll('li button');
+            const pageNumber = e.target.textContent;
+
+            for (let i = 0; i < linkListButtons.length; i++) {
+                linkListButtons[i].classList.remove('active');
+                e.target.classList.add('active');
+                showPage(recipes, +pageNumber);
+            }
         }
     });
+}
+
+// Page is 1 by default and list is the data array of students.
+function showPage(list, page) {
+    const startIndex = (page * 9) - 9;
+    const endIndex = page * 9;
+    const studentList = document.querySelector('.recipes-img');
+    studentList.innerHTML = '';
+
+    for (let i = 0; i < list.length; i++) {
+        if (i >= startIndex && i < endIndex) {
+            studentList.insertAdjacentHTML("beforeend",
+                `<div class="featured-img-pic">
+                    <div class="recipe-container">
+                        <a href="#">
+                            <img alt=${list[i].name} src=${list[i].image}>
+                        </a>
+                    <div class="recipe-overlay">
+                        <div class="recipe-overlay-text">
+                            <p>${list[i].name}</p>
+                            <div class="recipe-buttons">
+                                <a href="#"><button class="recipe-button" data-index=${list[i].id}>Recipe</button></a>
+                                <a href=${list[i].link} target="_blank"><button
+                                        class="recipe-button">Video</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <span class="recipe">${list[i].name}</span>`);
+        }
+    }
+
+    const recipeButton = document.querySelectorAll('.recipe-button');
+    recipeButton.forEach((button) => {
+        button.addEventListener('click', displayRecipe);
+    });
+
+    return studentList;
 }
 
 // Display recipe to recipe.html without changing to a new page
@@ -101,21 +179,33 @@ function displayRecipe(e) {
 }
 
 
+
 /* ============================================= */
 /*              Event Listeners                  */
 /* ============================================= */
 
-
+// open nav
 navMenu.addEventListener('click', openNav);
+
+// close nav
 window.addEventListener('resize', closeNavOnWindowResize);
+
+// close nav
 closeBtn.addEventListener('click', () => {
     closeNav();
     navMenu.style.display = "block";
 });
+
+// change nav styling when scrolling
 window.addEventListener('scroll', blackNavOnScroll);
+
+// search for recipes while typing
 if (recipesSearch) {
     recipesSearch.addEventListener('keyup', searchRecipes);
 }
+
+// display recipe
+const recipeButton = document.querySelectorAll('.recipe-button');
 recipeButton.forEach((button) => {
     button.addEventListener('click', displayRecipe);
-})
+});
